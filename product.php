e604ecb5fbf70898b222cdbbc5716672ec0f9f4a
Nick <61029188+nickadaniel@users.noreply.github.com>
2022-02-25 11:23:24 -0800

    Add files via upload

---
+++ product.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php 
+session_start();
+// initializing variables 
+$username = "";
+    email = "";
+    $errors = array();
+
+// connnect to the database
+$db = mysql_connect('localhost', 'root', '', 'project');
+
+if ($db->connect_error) 
+{
+    die("connectoin failed: " .$db->connect_error);
+}
+
+$sql = "SELECT id, username, email,img FROM users";
+$result = $db->query($sql);
+
+
+if ($result->num_rows > 0) {
+    //output data each row 
+    while($row = $result->fetch_assoc()) {
+        print "" <br> id: ". $row[id"]. "<br> - Name: ". 
+
+$row ["username"]. "<br> - Email: " . $row["email"] . "<br>";
+print "<img src=\"" .$row["img"]."/">;
+}
+} else
+{
+    print "0 results";
+}
+
+$db->close();
+?>
+
+</body>
+</html>
